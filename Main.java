08229644d9edba99f1259508eb528927a704ffc8
Reemas Alyahia <130400514+mime2001@users.noreply.github.com>
2024-10-21 09:15:29 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hi every one i'm Reemas Alyahia ");
+        System.out.println("i'm freash gradute from prinnce sattam bin Abdualaziz");
+        System.out.println("and i coming here becose there pure experince here also there alote of pepole that thay want to make you better");
+
+        }
+    }
